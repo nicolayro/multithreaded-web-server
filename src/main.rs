7f5d40f4610355b7f2877afe846b542160e4cf67
Nicolay Roness <nicroness@gmail.com>
2024-06-27 22:10:03 +0200

    add single threaded web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::{
+    fs, io::{prelude::*, BufReader}, net::{TcpListener, TcpStream}, thread, time::Duration
+};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => handle_stream(stream),
+            Err(err) => eprintln!("Error while connection to stream: {}", err)
+        }
+    }
 }
+
+fn handle_stream(mut stream: TcpStream) {
+    println!("Connected!");
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "index.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "index.html")
+        },
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    stream.write_all(response.as_bytes()).unwrap();
+}
+
